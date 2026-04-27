02ae90b6d5c45123cf9bb7166484ea022609e85c
Sourav-Ghosh10 <souravghoshmgu1@gmail.com>
2026-04-27 16:14:53 +0530

    mail isue

--- src/config/email.js
+++ src/config/email.js
@@ -8,7 +8,8 @@ const createEmailTransporter = () => {
     transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: parseInt(process.env.EMAIL_PORT, 10),
-      secure: parseInt(process.env.EMAIL_PORT, 10) === 465, // true for 465, false for other ports
+      // secure: parseInt(process.env.EMAIL_PORT, 10) === 465, // true for 465, false for other ports
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
